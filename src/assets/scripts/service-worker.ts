c471e86a56108d2fc901d13be62f4b6c1a9d497c
Hung Le <hung.le.kim@ericsson.com>
2023-10-05 10:40:51 +0700

    remove delay

--- src/assets/scripts/service-worker.ts
+++ src/assets/scripts/service-worker.ts
@@ -1,14 +1,3 @@
-// Slow the serviceworker down a bit
-const start = Date.now();
-let i = 0;
-while (Date.now() - start < 5000) {
-  i++;
-};
-
-console.log(i);
-console.log('delay done');
-console.log('installing');
-
 self.addEventListener('install', (event: FetchEvent) => {
   console.log('install');
 });
