30e5132cd2db5e58ed1dcd418b6b2478af4fcb74
Hung Le <hung.le.kim@ericsson.com>
2023-10-05 16:30:56 +0700

    add console log

--- src/assets/scripts/register-service-worker.ts
+++ src/assets/scripts/register-service-worker.ts
@@ -8,12 +8,16 @@ declare global {
 const registerServiceWorker = async () => {
   if('serviceWorker' in navigator) {
     try {
+      console.log("Service worker registering");
+
       const registration = await navigator.serviceWorker.register(`${import.meta.env.BASE_URL}service-worker.js`);
 
       console.log("Service worker registered");
     } catch(error) {
       console.log(error);
     }
+  } else {
+    console.log('SW is not supported');
   }
 }
 
